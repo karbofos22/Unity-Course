3cbf191ced4ffaa9cb15516952ecf0c305f7b4aa
Karbofos <karbofos22@yandex.ru>
2022-08-17 18:46:14 +0300

    Added homework_2 files

---
+++ HomeWork_2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using static HomeWork_2.Shooter;
+
+namespace HomeWork_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Rpg ragnar = new("Ragnar", 15, 30);
+            ragnar.ShowInfo();
+
+            Shooter levelSubBase = new(Weapons.MP5, 3, 1, "Hi-Explosive Bomb", "Infiltrate sub-marine base, steal launch codes and destroy mini-submarine prototype");
+            levelSubBase.ShowInfo();
+
+            Strategy abandonedOutpost = new(10, 100, 50, 5, 0);
+            abandonedOutpost.ShowInfo();
+
+            Moba game = new();
+            game.ShowInfo();
+
+            Puzzle oldCave = new(1, 0, 25, 5);
+            oldCave.ShowInfo();
+        }
+    }
+}
---
+++ HomeWork_2/Strategy.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork_2
+{
+    internal class Strategy
+    {
+        readonly int workers = 10;
+        private int wood;
+        private int stone;
+        private int money;
+        readonly int enemyCount;
+
+        public Strategy(int workers, int wood, int stone, int enemyCount, int money)
+        {
+            this.workers = workers;
+            this.wood = wood;
+            this.stone = stone;
+            this.enemyCount = enemyCount;
+            this.money = money;
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"Objectives:\n" +
+                              $"Rebuild barracks by using provided resources - {wood} wood, {stone} stone.\n" +
+                              $"Send your {workers} workers to harwest wood and stone, sell it at market.\n" +
+                              $"Use money(currently {money}) to hire 10 soldiers.\n" +
+                              $"Kill {enemyCount} werewolves at nothern kingdom border.\n");
+        }
+    }
+}
+   
