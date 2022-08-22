6f67b479cbc0b73588e72fc11d51f24788c48c4e
Karbofos <karbofos22@yandex.ru>
2022-08-22 18:34:39 +0300

    added HomeWork_3 files

---
+++ HomeWork_3/Dwarf.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork_3
+{
+    internal class Dwarf : PlayableCharacter
+    {
+        readonly bool ironHead = true;
+        readonly bool stoneSkin = true;
+        readonly string homeLand = "Grey Mountains";
+
+
+        public Dwarf(string name) : base()
+        {
+            Name = name;
+            Strength = 7;
+            Agility = 2;
+            Endurance = 15;
+            Description = DescriptionGenerator();
+        }
+        public override void ShowInfo()
+        {
+            base.ShowInfo();
+            Console.WriteLine("Race perks:\n" +
+                              "\tIron head - cant be confused or knocked out\n" +
+                              "\tStone skin - permanent work in the mine hardens you");
+
+        }
+        protected override string DescriptionGenerator()
+        {
+            return $"{base.DescriptionGenerator()} dwarf of {homeLand}";
+        }
+        protected override int CalcTotalHp()
+        {
+            if (stoneSkin)
+            {
+                base.hpRate = 5;
+            }
+            return base.CalcTotalHp();
+        }
+
+    }
+}
---
+++ HomeWork_3/PlayableCharacter.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork_3
+{
+    internal abstract class PlayableCharacter
+    {
+        protected string Name { get; set; }
+        protected string Description { get; set; }
+        protected int Strength { get; set; }
+        protected int Agility { get; set; }
+        protected int Endurance { get; set; }
+
+
+
+        // Stats modoficators
+        protected int damageRate = 3;
+        protected int hpRate = 3;
+        protected int evadeRate = 3;
+
+        //Additional stuff
+        protected string[] words = new[] { "mighty", "fearsome", "tough", "fearsome" };
+        protected string[] words1 = new[] { "warrior", "fighter", "battler", "champion" };
+
+
+        protected virtual int CalcDamageOutput()
+        {
+            return Strength * damageRate;
+        }
+        protected virtual int CalcTotalHp()
+        {
+            return Endurance * hpRate;
+        }
+        protected virtual int EvadeChance()
+        {
+            return Agility * evadeRate;
+        }
+        public virtual void ShowInfo()
+        {
+            Console.WriteLine($"Hero stats:\n" +
+                              $"\tName: {Name}\n" +
+                              $"\tStrenght: {Strength} (Damage output {CalcDamageOutput()})\n" +
+                              $"\tAgility: {Agility} (Evade chance {EvadeChance()}%)\n" +
+                              $"\tEndurance: {Endurance} (HP amount {CalcTotalHp()})\n" +
+                              $"\tDescription: {Description}");
+        }
+        protected virtual string DescriptionGenerator()
+        {
+            Random random = new();
+            return $"{words[random.Next(0, words.Length)]} {words1[random.Next(0, words.Length)]}";
+        }
+
+
+    }
+}
